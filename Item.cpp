2fd8be0a372dbbcbd139879d49807871b345aa20
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2025-11-20 13:12:49 +0100

    initial commit

---
+++ Item.cpp
@@ -0,0 +1,13 @@
+#include "Item.h"
+
+void Item::setName(std::string newName) {
+    name = newName;
+}
+
+void Item::setQuantity(int newQuantity) {
+    quantity = newQuantity;
+}
+
+void Item::setPrice(double newPrice) {
+    price = newPrice;
+}
