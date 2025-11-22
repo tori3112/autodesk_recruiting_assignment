1c4be73ea38193099dc21cafc579db3493f36b27
Wiktoria Radecka <wiktorialaura.radecka@student.kuleuven.be>
2025-11-22 15:50:13 +0100

    working on more efficient version now

--- main.cpp
+++ main.cpp
@@ -1,13 +1,38 @@
-#include "electronics.h"
-#include "groceries.h"
 #include "inventory.h"
 #include <iostream>
 #include <sstream>
+#include <cctype>
 
-void printHelp() {
+bool canBeInteger(std::string& str) {
+    std::string numbers = "1234567890";
+    for (const auto& ch : str) {
+        if (numbers.find(ch) == std::string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool canBeDouble(std::string& str) {
+    int decimal = 0;
+    for (auto c : str) {
+        if (!std::isdigit(c)) {
+            if (c == '.') {
+                decimal++;
+                if (decimal > 1) return false;
+            } else {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void printHelp() { // TODO: improve commands instructions
     std::cout << "Commands:\n"
               << "\tadd\t\t - add an item,\n"
               << "\tremove\t\t - removes an item by IT,\n"
+              << "\tupdate\t\t - updates quantity of an item, \n"
               << "\tdisplay\t\t - shows all items in the invetory,\n"
               << "\tread\t\t - reads items from file,\n"
               << "\tsave\t\t - saves current inventory to a file,\n"
@@ -46,41 +71,57 @@ int main()
         } else if (line[0] == "add") {
             if (line.size() == 3) {
                 // add <item name> <item price>
-                std::string name = line[1];
-                double price = std::stod(line[2]);
-                auto item = std::make_shared<Item>(name, price);
-                inventory.addItem(item);
+                auto name = line[1];                // expect std::string
+                auto price = line[2];               // expect double
+
+                if (canBeDouble(price)) {
+                    auto item = std::make_shared<Item>(name, std::stod(price));
+                    inventory.addItem(item);
+                } else {
+                    std::cout << "Given price in unrecognised standard: " << price << std::endl;
+                }
+
             } else if (line.size() == 4) {
                 // add <item name> <item quantity> <item price>
-                std::string name = line[1];
-                int quantity = std::stoi(line[2]);
-                double price = std::stod(line[3]);
-                auto item = std::make_shared<Item>(name, quantity, price);
-                inventory.addItem(item);
+                auto name = line[1];                // expect std::string
+                auto quantity = line[2]; // expect int
+                auto price = line[3];    // expect double
+
+                if (canBeInteger(quantity)) {
+                    if (canBeDouble(price)) {
+                        auto item = std::make_shared<Item>(name, std::stoi(quantity), std::stod(price));
+                        inventory.addItem(item);
+                    } else {
+                        std::cout << "Given price in unrecognised standard: " << price << std::endl;
+                    }
+                } else {
+                    std::cout << "Given quantity in unrecognised standard: " << price << std::endl;
+                }
             } else {
                 std::cout << "Wrong number of arguments." << std::endl;
             }
         } else if (line[0] == "remove") {
             if (line.size() != 2) {
                 std::cout << "Wrong number of arguments." << std::endl;
+            } else {
+                auto id = line[1];
+                inventory.removeItem(id);
             }
-
-            auto id = line[1];
-            inventory.removeItem(id);
         } else if (line[0] == "display") {
             inventory.display();
         } else if (line[0] == "read") {
-
-        } else if (line[0] == "save") {
-            std::vector<std::string> line;
-            std::string word;
-
-            while (std::getline(ss, word, ' ')) {
-                line.push_back(word);
-            }
-
-            if (line.size() < 2) {
+            if (line.size() != 2) {
                 std::cout << "Please specify target file." << std::endl;
+            } else {
+                auto filename = line[1];
+                inventory.readFile(filename);
+            }
+        } else if (line[0] == "save") {
+            if (line.size() != 2) {
+                std::cout << "Please specify target file." << std::endl;
+            } else {
+                auto filename = line[1];
+                inventory.saveToFile(filename);
             }
         } else if (line[0] == "highest") {
             auto highest = inventory.findHighestPrice();
@@ -89,15 +130,19 @@ int main()
             // TODO: what if user puts threshold as a non-numeric
             if (line.size() != 2) {
                 std::cout << "Wrong number of arguments." << std::endl;
+            } else {
+                auto threshold = line[1];
+                if (canBeDouble(threshold)) {
+                    auto items = inventory.findItemsBelowThreshold(std::stod(threshold));
+                    std::cout << "Found " << items.size() << " items below " << threshold << std::endl;
+                    for (const auto& item : items) {
+                        std::cout << item->getInfo();
+                    }
+                    std::cout << "DONE." << std::endl;
+                } else {
+                    std::cout << "Unnexpected threshold value: " << threshold << std::endl;
+                }
             }
-
-            auto threshold = std::stod(line[1]);
-            auto items = inventory.findItemsBelowThreshold(threshold);
-            std::cout << "Found " << items.size() << " items below " << threshold << std::endl;
-            for (const auto& item : items) {
-                std::cout << item->getInfo();
-            }
-            std::cout << "DONE." << std::endl;
         } else if (line[0] == "exit") {
             break;
         }
